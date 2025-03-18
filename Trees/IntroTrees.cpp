8cb8ff000606873b80692e61a77dc5c2757269b2
Your Name <divyank1234@gmail.com>
2025-03-18 20:42:06 +0530

    Trees 18/3/25

---
+++ Trees/IntroTrees.cpp
@@ -0,0 +1,42 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+// } Driver Code Ends
+
+
+// } Driver Code Ends
+
+// User function Template for C++
+
+class Solution {
+  public:
+    int countNodes(int i) {
+        // your code here
+        i = pow(2, i-1);
+        return i;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int i;
+        cin >> i;
+        Solution ob;
+        int res = ob.countNodes(i);
+
+        cout << res;
+        cout << "\n";
+    
+cout << "~" << "\n";
+}
+}
+
+// } Driver Code Ends

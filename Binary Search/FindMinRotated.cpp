114a3c3faa20af88924e82c6f40c2950da938532
Your Name <divyank1234@gmail.com>
2025-03-24 20:10:41 +0530

    Peak Element BS 24/3/25

---
+++ Binary Search/FindMinRotated.cpp
@@ -0,0 +1,24 @@
+class Solution {
+    public:
+        int findMin(vector<int>& nums) {
+            int ans =  INT_MAX;
+            int low = 0 ;
+            int high = nums.size()-1;
+            while(low <= high){
+                int mid = (low + high)/2;
+                if(nums[low] <= nums[mid]){
+                    if(nums[low] < ans){
+                        ans = nums[low];
+                    }
+                    low = mid + 1;
+                }
+                else{
+                    if(nums[mid] < ans){
+                        ans = nums[mid];
+                    }
+                    high = mid - 1;
+                }
+            }
+            return ans;
+        }
+    };
---
+++ Binary Search/HowManyTimesRotated.cpp
@@ -0,0 +1,63 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+// } Driver Code Ends
+
+// User function template for C++
+class Solution {
+  public:
+    int findKRotation(vector<int> &arr) {
+        // Code Here
+        int ans = INT_MAX;
+        int low = 0, high = arr.size() - 1;
+        int index;
+        if(arr[low] <= arr[high]) return 0;
+        while(low <= high){
+            int mid = (low + high)/2;
+                if(arr[low] <= arr[mid]){
+                    if(arr[low] < ans){
+                        ans = arr[low];
+                        index = low;
+                    }
+                    low = mid + 1;
+                }
+                else{
+                    if(arr[mid] < ans){
+                        ans = arr[mid];
+                        index = mid;
+                    }
+                    high = mid - 1;
+                }
+        }
+        return index;
+    }
+};
+
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<int> arr;
+        string input1;
+        getline(cin, input1);
+        stringstream ss1(input1);
+        int number1;
+        while (ss1 >> number1) {
+            arr.push_back(number1);
+        }
+        Solution ob;
+        int res = ob.findKRotation(arr);
+        cout << res << endl;
+        cout << "~" << endl;
+    }
+}
+
+// } Driver Code Ends
